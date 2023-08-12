764e2d665826ed39a6a990a8d584b6d5ab4a5719
Cootakahashi <saiko.life2020@gmail.com>
2023-08-13 02:21:08 +0900

    added next.config for image domains

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost:8000", "127.0.0.1", "localhost"], // 開発環境でのDjangoサーバーのドメインを設定します。
+    domains: ["djangobackec-f0d32835962b.herokuapp.com"],
+    // domains: ["localhost:8000", "127.0.0.1", "localhost"], // 開発環境でのDjangoサーバーのドメインを設定します。
   },
 };
